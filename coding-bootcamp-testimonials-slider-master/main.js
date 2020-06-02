e64a968099e2c8b82c31d9d557aac934dd504cc0
Shaifullah Arfan <shaifullaharfan17@gmail.com>
2020-06-02 06:54:29 +0600

    final for testimonial slider

--- coding-bootcamp-testimonials-slider-master/main.js
+++ coding-bootcamp-testimonials-slider-master/main.js
@@ -1,32 +1,29 @@
-const next = buttons_div.querySelector('.next');
-const prev = buttons_div.querySelector('.prev');
-const slides = document.querySelectorAll('.slider .slide');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
 let index = 0;
-
-display(0);
-
+display(index);
 function display (index) {
 	slides.forEach((slide) => {
 		slide.style.display = 'none';
-		slides[index].style.display = 'flex';
 	});
+	slides[index].style.display = 'flex';
 }
+
 function nextSlide () {
 	index++;
 	if (index > slides.length - 1) {
 		index = 0;
 	}
 	display(index);
-	console.log(slides);
 }
-
 function prevSlide () {
 	index--;
 	if (index < 0) {
 		index = slides.length - 1;
 	}
 	display(index);
-	console.log(slides);
 }
 
 next.addEventListener('click', nextSlide);
